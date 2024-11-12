b10922ed6993d76945b69e8513c0c2d2ed57e943
Sevag <baghsev@gmail.com>
2024-11-11 22:44:27 -0500

    Hook up relay server

---
+++ app/index.js
@@ -0,0 +1,28 @@
+// import { RealtimeClient } from "@openai/realtime-api-beta";
+
+// const client = new RealtimeClient({ apiKey: process.env.OPENAI_API_KEY });
+
+// // Can set parameters ahead of connecting, either separately or all at once
+// client.updateSession({ instructions: "You are a great, upbeat friend." });
+// client.updateSession({ voice: "alloy" });
+// client.updateSession({
+//   turn_detection: { type: "server_vad" }, // or 'server_vad'
+//   input_audio_transcription: { model: "whisper-1" },
+// });
+
+// // Set up event handling
+// client.on("conversation.updated", (event) => {
+//   const { item, delta } = event;
+//   const items = client.conversation.getItems();
+//   /**
+//    * item is the current item being updated
+//    * delta can be null or populated
+//    * you can fetch a full list of items at any time
+//    */
+// });
+
+// // Connect to Realtime API
+// await client.connect();
+
+// // Send a item and triggers a generation
+// client.sendUserMessageContent([{ type: "input_text", text: `How are you?` }]);
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,144 @@
-import Image from "next/image";
+"use client";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+
+import { RealtimeClient } from "@openai/realtime-api-beta";
+import { ItemType } from "@openai/realtime-api-beta/dist/lib/client.js";
+import { WavRecorder, WavStreamPlayer } from "./lib/wavtools/index.js";
+
+import { TurnDetectionServerVadType } from "@openai/realtime-api-beta/dist/lib/client.js";
+
+const LOCAL_RELAY_SERVER_URL: string =
+  process.env.NEXT_PUBLIC_LOCAL_RELAY_SERVER_URL || "";
+console.log("LOCAL RELAY URL:", LOCAL_RELAY_SERVER_URL);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [items, setItems] = useState<ItemType[]>([]);
+  const [isConnected, setIsConnected] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // reference to openAI client
+  const clientRef = useRef<RealtimeClient>(
+    new RealtimeClient({
+      url: LOCAL_RELAY_SERVER_URL,
+    })
+  );
+
+  // reference to speech input
+  const wavRecorderRef = useRef<WavRecorder>(
+    new WavRecorder({ sampleRate: 24000 })
+  );
+
+  // reference to speech output
+  const wavStreamPlayerRef = useRef<WavStreamPlayer>(
+    new WavStreamPlayer({ sampleRate: 24000 })
+  );
+
+  // conversation connection
+  const connectConversation = useCallback(async () => {
+    const client = clientRef.current;
+    const wavRecorder = wavRecorderRef.current;
+    const wavStreamPlayer = wavStreamPlayerRef.current;
+
+    client.updateSession({
+      turn_detection: { type: "server_vad" },
+      instructions:
+        "You are a supervisor at a company meant to interview a candidate. Your name is John. Always reply with audio.",
+    });
+
+    setItems(client.conversation.getItems());
+
+    // connect to microphone
+    await wavRecorder.begin();
+
+    // connect to audio output
+    await wavStreamPlayer.connect();
+
+    // connect to realtime API
+    await client.connect();
+
+    setIsConnected(true);
+
+    client.sendUserMessageContent([
+      {
+        type: `input_text`,
+        text: `Hello! My name is Sevag.`,
+      },
+    ]);
+
+    console.log(client);
+
+    if (client.getTurnDetectionType() === "server_vad") {
+      await wavRecorder.record((data) => client.appendInputAudio(data.mono));
+    }
+  }, []);
+
+  const disconnectConversation = useCallback(async () => {
+    const client = clientRef.current;
+    const wavRecorder = wavRecorderRef.current;
+    const wavStreamPlayer = wavStreamPlayerRef.current;
+
+    client.disconnect();
+    setIsConnected(false);
+
+    await wavRecorder.end();
+    await wavStreamPlayer.interrupt();
+  }, []);
+
+  useEffect(() => {
+    const client = clientRef.current;
+    const wavStreamPlayer = wavStreamPlayerRef.current;
+
+    client.updateSession({ input_audio_transcription: { model: "whisper-1" } });
+
+    client.on("error", (event: any) => console.error(event));
+    client.on("conversation.interrupted", async () => {
+      const trackSampleOffset = await wavStreamPlayer.interrupt();
+      if (trackSampleOffset?.trackId) {
+        const { trackId, offset } = trackSampleOffset;
+        await client.cancelResponse(trackId, offset);
+      }
+    });
+
+    client.on("conversation.updated", async ({ item, delta }: any) => {
+      const items = client.conversation.getItems();
+      if (delta?.audio) {
+        wavStreamPlayer.add16BitPCM(delta.audio, item.id);
+      }
+      if (item.status === "completed" && item.formatted.audio?.length) {
+        const wavFile = await WavRecorder.decode(
+          item.formatted.audio,
+          24000,
+          24000
+        );
+        item.formatted.file = wavFile;
+      }
+      setItems(items);
+    });
+
+    setItems(client.conversation.getItems());
+
+    return () => {
+      client.reset();
+    };
+  }, []);
+
+  console.log("ITEMS:", items);
+
+  return (
+    <div className="">
+      <button
+        className="bg-gray-200 p-2 m-2 rounded-sm"
+        onClick={isConnected ? disconnectConversation : connectConversation}
+      >
+        {isConnected ? "Disconnect" : "Connect conversation"}
+      </button>
+      {items.map((conversationItem, i) => {
+        return (
+          conversationItem.formatted.file && (
+            <audio key={i} src={conversationItem.formatted.file.url} controls />
+          )
+        );
+      })}
     </div>
   );
 }
